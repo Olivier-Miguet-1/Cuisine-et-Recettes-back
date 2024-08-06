f1354e3ea576596ce505d22c8c677893c1182fdf
Olivier-Miguet-1 <olivier.miguet1@gmail.com>
2024-08-06 11:50:15 +0200

    Ingredient and Utensil in index.js

--- tests/index.js
+++ tests/index.js
@@ -19,6 +19,22 @@ describe("RecipeController", () => {
     require('./controllers/RecipeController.test')
 })
 
+describe("IngredientService", () => {
+    require('./services/IngredientService.test')
+})
+
+describe("IngredientController", () => {
+    require('./controllers/IngredientController.test')
+})
+
+describe("UtensilService", () => {
+    require('./services/UtensilService.test')
+})
+
+describe("UtensilController", () => {
+    require('./controllers/UtensilController.test')
+})
+
 describe("API - Mongo", () => {
     it("vider les dbs. - S", () => {
         if (process.env.npm_lifecycle_event == 'test')
